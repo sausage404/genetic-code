8e9b0be3e99203795aa0af75ba0624e2e79996c2
MSC BEEHI <112796660+MrMaxing@users.noreply.github.com>
2023-12-14 11:23:10 +0700

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -53,31 +53,29 @@ export default function App() {
                     </div>
                     <div className="flex flex-wrap max-w-screen-xl justify-center">
                         {res.map((v, i) => (
-                            <div key={i} className="flex items-center px-2">
-                                <div className="rounded-md border border-gray-300 p-4 m-4 bg-gray-50 w-full">
-                                    <button
-                                        className="float-right bg-orange-500 text-white rounded-md p-2 hover:bg-orange-600 transition duration-300 ease-in-out focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-offset-gray-300 focus:ring-white"
-                                        onClick={() => setRes((prevRes) => prevRes.filter((_, index) => index !== i))}
-                                    >
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
-                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-                                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
-                                        </svg>
-                                    </button>
-                                    <h4 className="text-xl font-bold mb-3">Result {i + 1}</h4>
-                                    <p className="text-md font-semibold"><span className="font-bold">DNA</span> ที่ป้อน</p>
-                                    <p className="text-md mb-3 break-normal">3' {v.DNA} 5'</p>
-                                    <p className="text-md font-semibold"><span className="font-bold">mRNA</span> ที่แปลง จาก DNA</p>
-                                    <p className="text-md mb-3 break-normal">5' {v.mRNA} 3'</p>
-                                    <p className="text-md font-semibold"><span className="font-bold">tRNA</span> ที่แปลง จาก mRNA</p>
-                                    <p className="text-md mb-3 break-normal">3' {v.tRNA} 5'</p>
-                                    <p className="text-md font-semibold"><span className="font-bold">Codons</span> ที่แปลง จาก mRNA</p>
-                                    <p className="text-md mb-3 break-normal">{v.codons}</p>
-                                    <p className="text-md font-semibold"><span className="font-bold">จำนวน</span> Codons</p>
-                                    <p className="text-md mb-3 break-normal">{v.count} ตัว</p>
-                                    <p className="text-md font-semibold"><span className="font-bold">Amino</span> ที่แปลง จาก Codons</p>
-                                    <p className="text-md mb-3 break-normal">{v.amino}</p>
-                                </div>
+                            <div key={i} className="rounded-md border border-gray-300 p-4 m-4 bg-gray-50 block break-all">
+                                <button
+                                    className="float-right bg-orange-500 text-white rounded-md p-2 hover:bg-orange-600 transition duration-300 ease-in-out focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-offset-gray-300 focus:ring-white"
+                                    onClick={() => setRes((prevRes) => prevRes.filter((_, index) => index !== i))}
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+                                    </svg>
+                                </button>
+                                <h4 className="text-xl font-bold mb-3">Result {i + 1}</h4>
+                                <p className="text-md font-semibold"><span className="font-bold">DNA</span> ที่ป้อน</p>
+                                <p className="text-md mb-3">3' {v.DNA} 5'</p>
+                                <p className="text-md font-semibold"><span className="font-bold">mRNA</span> ที่แปลง จาก DNA</p>
+                                <p className="text-md mb-3">5' {v.mRNA} 3'</p>
+                                <p className="text-md font-semibold"><span className="font-bold">tRNA</span> ที่แปลง จาก mRNA</p>
+                                <p className="text-md mb-3">3' {v.tRNA} 5'</p>
+                                <p className="text-md font-semibold"><span className="font-bold">Codons</span> ที่แปลง จาก mRNA</p>
+                                <p className="text-md mb-3">{v.codons}</p>
+                                <p className="text-md font-semibold"><span className="font-bold">จำนวน</span> Codons</p>
+                                <a className="text-md mb-3">{v.count} ตัว</a>
+                                <p className="text-md font-semibold"><span className="font-bold">Amino</span> ที่แปลง จาก Codons</p>
+                                <a className="text-md mb-3">{v.amino}</a>
                             </div>
                         ))}
                     </div>
